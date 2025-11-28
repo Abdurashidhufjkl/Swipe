34a40d5dc4c14872258171d88393246a3d6e0017
abdurashidhufjkl <abdurashidrashimov@gmail.com>
2025-11-28 16:23:04 +0500

    swipe

---
+++ main.js
@@ -0,0 +1,24 @@
+let dark = document.querySelector(".dark");
+let light = document.querySelector(".light");
+
+let elements = document.querySelectorAll("p, a, li, h1, h2, h3, span");
+
+dark.addEventListener("click", (e) => {
+  e.preventDefault();
+  document.body.style.backgroundColor = "#121212";
+  document.body.style.color = "white";
+
+  elements.forEach((el) => {
+    el.style.color = "white";
+  });
+});
+
+light.addEventListener("click", (e) => {
+  e.preventDefault();
+  document.body.style.backgroundColor = "white";
+  document.body.style.color = "black";
+
+  elements.forEach((el) => {
+    el.style.color = "black";
+  });
+});
