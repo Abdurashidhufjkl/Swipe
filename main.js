cdae3e5fb09817be8fdeb5998b441ca405afa1d5
abdurashidhufjkl <abdurashidrashimov@gmail.com>
2025-12-01 17:20:53 +0500

    color

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let dark = document.querySelector(".dark");
 let light = document.querySelector(".light");
 let hover = document.querySelectorAll(".footer a");
-let elements = document.querySelectorAll("p, a, li, h1, h2, h3, h4, span");
+let elements = document.querySelectorAll("p, a, li, h1, h2, h3, h4, span, label");
 
 dark.addEventListener("click", (e) => {
   e.preventDefault();
